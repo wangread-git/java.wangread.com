3a8d64b9d55665aed04e24106fb431cd6b6c1c78
wangread-git <wang.read@gmail.com>
2015-09-21 09:23:25 +0800

    google protobuf demo

---
+++ src/main/java/com/read/test/protobuf/ProtobufExample.java
@@ -0,0 +1,33 @@
+package com.read.test.protobuf;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.junit.Test;
+
+/**
+ * Created by yfwangrui on 2015/9/16.
+ *
+ * protobuf example
+ */
+public class ProtobufExample {
+
+    @Test
+    public void test() {
+        PersonProtos.Person.Builder personBuilder = PersonProtos.Person.newBuilder();
+        personBuilder.setEmail("test@gmail.com");
+        personBuilder.setName("test");
+        personBuilder.setId(1);
+        PersonProtos.Person.PhoneNumber.Builder phoneBuilder = PersonProtos.Person.PhoneNumber.newBuilder();
+        phoneBuilder.setNumber("123456789");
+        phoneBuilder.setType(PersonProtos.Person.PhoneType.MOBILE);
+        personBuilder.addPhone(phoneBuilder);
+        PersonProtos.Person person = personBuilder.build();
+
+        byte[] data = person.toByteArray();
+        System.out.println(new String(data));
+        try {
+            System.out.println(PersonProtos.Person.parseFrom(data));
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+    }
+}
