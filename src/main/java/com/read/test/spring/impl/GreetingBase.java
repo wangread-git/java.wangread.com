e6d39474398c7a343d616032df55c5ae7e044574
bjyfwangrui <yfwangrui@jd.com>
2016-07-05 18:15:03 +0800

    1. add kafka demo

---
+++ src/main/java/com/read/test/spring/impl/GreetingBase.java
@@ -0,0 +1,11 @@
+package com.read.test.spring.impl;
+
+/**
+ * Created by bjyfwangrui on 2016/4/5.
+ */
+public class GreetingBase {
+
+    public void foo() {
+        System.out.println("foo bar!");
+    }
+}
